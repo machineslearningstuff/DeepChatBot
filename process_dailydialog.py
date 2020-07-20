1b13d8eb41c500855012f8646f61169d294efbf3
machineslearningstuff <68132004+machineslearningstuff@users.noreply.github.com>
2020-07-21 07:55:54 +1000

    Update process_dailydialog.py
    More updates

--- process_dailydialog.py
+++ process_dailydialog.py
@@ -17,6 +17,12 @@ from nltk.util import everygrams
 
 
 
+def sent_list_debug(sen_list, idx):
+    for sent in sen_list[idx]:
+        print(sent)
+    print('------------------------------')
+    
+
 
 def clean(s):
     # Clean daily dialog dataset sentences, from: https://github.com/gmftbyGMFTBY/MultiTurnDialogZoo/blob/master/utils.py
@@ -58,6 +64,37 @@ def generate_vocab(files, vocab, cutoff=50000):
         pkl.dump((w2idx, idx2w), f)
     print(f'[!] Save the vocab into {vocab}, vocab_size: {len(w2idx)}')    
 
+
+def generate_data(file, c_size, test_ratio):
+    # Generate training and testing dialog data from given file.
+    #   file: contains dialogs per line
+    #   c_size: total number of sentences in context + target sentence
+    #   ratio:  test/total ratio
+    
+    data = []
+
+    with open(file, 'r', encoding='utf-8') as f:
+        for line in tqdm(f.readlines()):
+            seqs = line.split('__eou__')[:-1]
+            clean_seqs = []
+            
+            # Clean each sentence in the dialog line
+            for seq in seqs:
+                clean_seqs.append(clean(seq))
+
+            # Each c_size consecutive sentences will be our data
+            # c_size - 1 sentences will be the context of the target sentence at c_size
+            for i in range(len(seq) - c_size):
+                sentSlice = clean_seqs[i:i+c_size]
+                if len(sentSlice) != c_size: continue
+                data.append(sentSlice)
+
+    print('Total data: %d' %len(data))
+
+    # Debug some random data
+    sent_list_debug(data, 156)
+    sent_list_debug(data, 1569)
+    sent_list_debug(data, 15634)
     
     
 def main():    
@@ -82,6 +119,10 @@ def main():
     with open('./vocab.pkl', 'rb') as f:
         w2idx, idx2w = pkl.load(f)
         print(idx2w[0:10])
+        
+    # Now start generating train and test data sentence lists
+    generate_data(args.file[0], args.ctx, 0.1)
+        
 
 if __name__ == "__main__":
     main()
